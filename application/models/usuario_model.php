0e6884ae72e586c62eb4fa01f04eb5509e78767f
yatotaus <yatotaus@gmail.com>
2016-06-18 19:45:31 -0300

    Update usuario_model.php

--- application/models/usuario_model.php
+++ application/models/usuario_model.php
@@ -23,5 +23,42 @@ class Usuario_model extends CI_Model {
       $consulta = $this->db->query($sql);
       return TRUE;
    }
+   
+   function ver_perfil($id_usuario) {
+    $this->db->select('*');
+    $this->db->where('usuarios.idUsuario', $id_usuario);
+    $this->db->from('usuarios');
+    $this->db->join('foto', 'usuarios.foto = foto.nombreFoto');
+    //$this->db->order_by("foto.idFoto", "asce");
+    $consulta = $this->db->get();
+    $cantidad = $consulta->num_rows();
+    if ($cantidad > 0) {
+      return $consulta;
+    } else {
+      return FALSE;
+    }
+  }  
+
+  public function modificar($id_usuario, $nombre) {
+    $sql = "SELECT * FROM `usuarios` WHERE `idUsuario` = $id_usuario and `nombre` = '$nombre'";
+    $consulta = $consulta = $this->db->query($sql);
+    $cantidad = $consulta->num_rows();
+    if ($cantidad == 1) {
+      return FALSE;
+    } else {
+      $query = "SELECT idUsuario FROM usuarios WHERE '$nombre' = UPPER(nombre)";
+      $result = $this->db->query($query);
+      $cant = $result->num_rows();
+      if($cant == 1) {
+        return 22;
+      } else {
+        $sql = "UPDATE `usuarios` SET `nombre` = '$nombre' WHERE `idUsuario` = $id_usuario";
+        $consulta = $this->db->query($sql);
+        return 7;
+      }          
+    }
+  }
+
+   
 }
 ?>
