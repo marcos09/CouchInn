e5f2b5f463ffa0ea7d3f801236c10b4db721d9fa
yatotaus <yatotaus@gmail.com>
2016-06-18 19:52:17 -0300

    Update header_premium.php

--- application/views/header_premium.php
+++ application/views/header_premium.php
@@ -263,7 +263,7 @@ VER EL USO DEL BODY
                  <li><a href="../index.php/couchs/publicar">Publicar couch</a></li>
                  <li><a href="#">Mis couchs</a></li>
                  <li><a href="#">Mis hospedajes</a></li>
-                 <li><a href="#">Modificar datos</a></li>
+                <li><a href="../index.php/inicio/ver_perfil/">Ver perfil</a></li>
                  <li><a href="#">Reservas</a></li>               
                  <li role="separator" class="divider"></li>
                  <!--      <li class="dropdown-header">Nav header</li> -->
